40dfad3edfd1cac40b94e972ab20549c1d3c1c9b
Rish-J <rishabhjhamnani@gmail.com>
2025-02-21 13:07:56 -0800

    Added skeleton to Controller.java

---
+++ Controller.java
@@ -0,0 +1,54 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class Controller {
+
+    @FXML
+    private Button roomsButton;
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
+    @FXML
+    void RoomsButtonSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void roomsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GuestsButtonSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guestsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StaffButtonSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void staffButton(ActionEvent event) {
+
+    }
+
+}
